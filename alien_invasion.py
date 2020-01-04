b233133d163114482fced16ce42e4d0181bf6d06
gspfilms <50781267+gspfilms@users.noreply.github.com>
2020-01-03 19:28:01 -0700

    added bullets and limits

--- alien_invasion.py
+++ alien_invasion.py
@@ -6,6 +6,8 @@ from settings import Settings
 
 from ship import Ship
 
+from bullet import Bullet
+
 class AlienInvasion:
     """Overall class to manage game assets and behavior."""
 
@@ -20,15 +22,29 @@ class AlienInvasion:
         pygame.display.set_caption("Alien Invasion")
 
         self.ship = Ship(self)
+        self.bullets = pygame.sprite.Group()
 
     def run_game(self):
         """Start the main loop for the game"""
         while True:
             self._check_events()
             self.ship.update()
+            self._update_bullets()
             self._update_screen()
-            # Watch for keyboard and mouse events.
 
+    def _update_bullets(self):
+            """Update position of bullets and get rid of old bullets"""
+            # Update bullet positions
+            self.bullets.update()
+
+            # Get rid of bullets the have disappeared.
+            for bullet in self.bullets.copy():
+                if bullet.rect.bottom <= 0:
+                    self.bullets.remove(bullet)
+            print(len(self.bullets))
+
+
+            # Watch for keyboard and mouse events.
     def _check_events(self):
         """Respond to keypresses and mouse events"""
         for event in pygame.event.get():
@@ -47,6 +63,8 @@ class AlienInvasion:
             self.ship.moving_left = True
         elif event.key == pygame.K_q:
             sys.exit()
+        elif event.key == pygame.K_SPACE:
+            self._fire_bullet()
 
     def _check_keyup_events(self, event):
         """Responds to key releases"""
@@ -55,11 +73,19 @@ class AlienInvasion:
         elif event.key == pygame.K_LEFT:
             self.ship.moving_left = False
 
+    def _fire_bullet(self):
+        """Create a new bullet and add it to the bullets group"""
+        if len(self.bullets) < self.settings.bullets_allowed:
+            new_bullet = Bullet(self)
+            self.bullets.add(new_bullet)
+
     def _update_screen(self):
         """Update images on thee screen, and flip to the new screen."""
         # Redraw the screen during each pass through the loop.
         self.screen.fill(self.settings.bg_color)
         self.ship.blitme()
+        for bullet in self.bullets.sprites():
+            bullet.draw_bullet()
 
         pygame.display.flip()
 
--- settings.py
+++ settings.py
@@ -10,3 +10,10 @@ class Settings:
 
         # Ship settings
         self.ship_speed = 3
+
+        # Bullet settings
+        self.bullet_speed = 10.0
+        self.bullet_width = 3
+        self.bullet_height = 15
+        self.bullet_color = (200, 200, 255)
+        self.bullets_allowed = 3
